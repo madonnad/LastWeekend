6c66567ad14b8015693c27d6aada4ee0e4b5b60b
Dominick Madonna <dominick.madonn@gmail.com>
2023-12-03 21:25:59 -0500

    Decided to reuse the old album creation screen and adapting it to the new design. Really the only piece that is being reused is the duration section - imported the added friends list and allowing the album to be created from the main page. Still need to add the friends screen.

--- lib/components/album_create_comp/date_time_card.dart
+++ lib/components/album_create_comp/date_time_card.dart
@@ -31,74 +31,91 @@ class DateTimeCard extends StatelessWidget {
       padding: const EdgeInsets.symmetric(vertical: 2),
       child: Card(
         elevation: 0,
-        color: const Color.fromRGBO(217, 217, 217, .25),
-        child: SizedBox(
-          height: 45,
-          width: 309,
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
+        color: const Color.fromRGBO(217, 217, 217, 1),
+        clipBehavior: Clip.hardEdge,
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 15.0),
+          decoration: const BoxDecoration(color: Color.fromRGBO(19, 19, 19, 1)),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              ShaderMask(
+                shaderCallback: (bounds) {
+                  return const LinearGradient(
+                    tileMode: TileMode.mirror,
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                    colors: [
+                      Color.fromRGBO(255, 205, 178, 1),
+                      Color.fromRGBO(255, 180, 162, 1),
+                      Color.fromRGBO(229, 152, 155, 1),
+                      Color.fromRGBO(181, 131, 141, 1),
+                      Color.fromRGBO(109, 104, 117, 1),
+                    ],
+                  ).createShader(bounds);
+                },
+                child: Text(
                   name,
                   style: GoogleFonts.poppins(
                     fontSize: 16,
-                    fontWeight: FontWeight.w500,
+                    fontWeight: FontWeight.w800,
+                    color: Colors.white,
                   ),
                 ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    TextButton(
-                      child: Text(
-                        dateText,
-                        style: GoogleFonts.poppins(
-                            fontSize: 16,
-                            fontWeight: FontWeight.w400,
-                            color: Colors.black45),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  TextButton(
+                    child: Text(
+                      dateText,
+                      style: GoogleFonts.poppins(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.white,
                       ),
-                      onPressed: () async {
-                        DateTime? pickedDate = await showDatePicker(
-                          context: context,
-                          initialDate: initialDate,
-                          firstDate: firstDate,
-                          lastDate: lastDate,
-                          currentDate: currentDate,
-                        );
-                        if (pickedDate != null) {
-                          dateCallback(pickedDate);
-                        } else {
-                          null;
-                        }
-                      },
                     ),
-                    TextButton(
-                      child: Text(
-                        timeText,
-                        style: GoogleFonts.poppins(
-                            fontSize: 16,
-                            fontWeight: FontWeight.w400,
-                            color: Colors.black45),
+                    onPressed: () async {
+                      DateTime? pickedDate = await showDatePicker(
+                        context: context,
+                        initialDate: initialDate,
+                        firstDate: firstDate,
+                        lastDate: lastDate,
+                        currentDate: currentDate,
+                      );
+                      if (pickedDate != null) {
+                        dateCallback(pickedDate);
+                      } else {
+                        null;
+                      }
+                    },
+                  ),
+                  TextButton(
+                    child: Text(
+                      timeText,
+                      style: GoogleFonts.poppins(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.white,
                       ),
-                      onPressed: () async {
-                        TimeOfDay? timePicked = await showTimePicker(
-                          initialEntryMode: TimePickerEntryMode.inputOnly,
-                          context: context,
-                          initialTime: TimeOfDay.now(),
-                        );
+                    ),
+                    onPressed: () async {
+                      TimeOfDay? timePicked = await showTimePicker(
+                        initialEntryMode: TimePickerEntryMode.inputOnly,
+                        context: context,
+                        initialTime: TimeOfDay.now(),
+                      );
 
-                        if (timePicked != null) {
-                          timeCallback(timePicked);
-                        } else {
-                          null;
-                        }
-                      },
-                    ),
-                  ],
-                ),
-              ],
-            ),
+                      if (timePicked != null) {
+                        timeCallback(timePicked);
+                      } else {
+                        null;
+                      }
+                    },
+                  ),
+                ],
+              ),
+            ],
           ),
         ),
       ),
