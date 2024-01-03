9ff1bdb4a4dbb866e9f9c00a742bc23cb8e87c58
Dominick Madonna <dominick.madonn@gmail.com>
2024-01-02 22:28:52 -0500

    Created the element that goes into the listview on the dialog, added the appropriate content and configured the logic for selecting and image and moving the page view on the image frame to the correct image via page controller. Did not create the element that shows the currently selected image on the list. Adding to todo list.

--- lib/screens/image_frame.dart
+++ lib/screens/image_frame.dart
@@ -1,5 +1,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:shared_photo/bloc/cubit/image_frame_cubit.dart';
+import 'package:shared_photo/components/image_page_comp/image_frame_container/image_frame_image_container.dart';
+import 'package:shared_photo/components/image_page_comp/image_frame_dialog/image_frame_dialog.dart';
 import 'package:shared_photo/models/image.dart' as img;
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -24,7 +26,7 @@ class ImageFrame extends StatelessWidget {
               child: SingleChildScrollView(
                 child: Padding(
                   padding: EdgeInsets.only(
-                    top: kToolbarHeight,
+                    top: MediaQuery.of(context).padding.top,
                     left: 10,
                     right: 10,
                     bottom: MediaQuery.of(context).padding.bottom,
@@ -33,102 +35,7 @@ class ImageFrame extends StatelessWidget {
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       const ImageFrameAppBar(),
-                      Container(
-                        height: MediaQuery.of(context).size.height * .6,
-                        decoration: const BoxDecoration(
-                          gradient: LinearGradient(
-                            begin: Alignment.topCenter,
-                            end: Alignment.bottomCenter,
-                            colors: [
-                              Colors.transparent,
-                              Color.fromRGBO(19, 19, 19, 1)
-                            ],
-                          ),
-                          borderRadius: BorderRadius.only(
-                            bottomLeft: Radius.circular(10),
-                            bottomRight: Radius.circular(10),
-                          ),
-                        ),
-                        child: Column(
-                          children: [
-                            Expanded(
-                              child: PageView.builder(
-                                controller: state.pageController,
-                                onPageChanged: (index) => context
-                                    .read<ImageFrameCubit>()
-                                    .imageChange(index),
-                                itemCount: state.selectedModeImages.length,
-                                itemBuilder: (context, index) {
-                                  return Padding(
-                                    padding: const EdgeInsets.all(8.0),
-                                    child: AspectRatio(
-                                      aspectRatio: 4 / 5,
-                                      child: Container(
-                                        decoration: BoxDecoration(
-                                          color: const Color.fromRGBO(
-                                              19, 19, 19, 1),
-                                          image: DecorationImage(
-                                              image: CachedNetworkImageProvider(
-                                                state.selectedModeImages[index]
-                                                    .imageReq,
-                                                headers: headers,
-                                              ),
-                                              fit: BoxFit.cover),
-                                          borderRadius:
-                                              BorderRadius.circular(10),
-                                        ),
-                                      ),
-                                    ),
-                                  );
-                                },
-                              ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.symmetric(
-                                horizontal: 25.0,
-                                vertical: 8,
-                              ),
-                              child: Row(
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                textBaseline: TextBaseline.alphabetic,
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Row(
-                                    children: [
-                                      const Icon(
-                                        Icons.favorite,
-                                        color: Colors.white,
-                                        size: 18,
-                                      ),
-                                      const SizedBox(
-                                        width: 5,
-                                      ),
-                                      Text(
-                                        state.selectedImage.upvotes.toString(),
-                                        style: GoogleFonts.josefinSans(
-                                          fontSize: 18,
-                                          color: Colors.white,
-                                          textBaseline: TextBaseline.alphabetic,
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                  Text(
-                                    state.selectedImage.dateString,
-                                    style: GoogleFonts.josefinSans(
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.w700,
-                                      color: Colors.white.withOpacity(.75),
-                                      textBaseline: TextBaseline.alphabetic,
-                                    ),
-                                  )
-                                ],
-                              ),
-                            )
-                          ],
-                        ),
-                      ),
+                      const ImageFrameImageContainer(),
                       Padding(
                         padding: const EdgeInsets.symmetric(vertical: 20.0),
                         child: Row(
@@ -148,13 +55,12 @@ class ImageFrame extends StatelessWidget {
                             GestureDetector(
                               onTap: () => showDialog(
                                 context: context,
-                                builder: (context) {
-                                  return Container(
-                                    height: 100,
-                                    width: 100,
-                                    color: Colors.red,
-                                  );
-                                },
+                                barrierColor: Colors.black87,
+                                builder: (ctx) => BlocProvider.value(
+                                  value:
+                                      BlocProvider.of<ImageFrameCubit>(context),
+                                  child: const ImageFrameDialog(),
+                                ),
                               ),
                               child: const Icon(
                                 Icons.view_timeline,
