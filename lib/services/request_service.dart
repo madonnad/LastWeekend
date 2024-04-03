22587149030ea1edf7368576f624ec33108b5322
Dominick Madonna <dominick.madonn@gmail.com>
2024-04-03 14:26:00 -0400

    Added the services that interact with the accepted and deny endpoints - will need to also add more endpoints in the future but these are just the starting ones.

--- lib/services/request_service.dart
+++ lib/services/request_service.dart
@@ -2,6 +2,7 @@ import 'package:shared_photo/utils/api_variables.dart';
 import 'package:http/http.dart' as http;
 
 class RequestService {
+  // Friend Requests
   static Future<bool> sendFriendRequest(String token, String uid) async {
     var url = Uri.http(domain, '/friend-request', {'id': uid});
     final Map<String, String> headers = {
@@ -68,6 +69,42 @@ class RequestService {
 
     try {
       final response = await http.patch(url, headers: headers);
+
+      if (response.statusCode == 200) {
+        return true;
+      }
+      return false;
+    } catch (e) {
+      print(e.toString());
+      return false;
+    }
+  }
+
+  // Album Invites
+
+  Future<bool> acceptAlbumInvite(String token, String requestID) async {
+    var url = Uri.http(domain, '/album-invite', {"id": requestID});
+    final Map<String, String> headers = {'Authorization': 'Bearer $token'};
+
+    try {
+      final response = await http.put(url, headers: headers);
+
+      if (response.statusCode == 200) {
+        return true;
+      }
+      return false;
+    } catch (e) {
+      print(e.toString());
+      return false;
+    }
+  }
+
+  Future<bool> denyAlbumInvite(String token, String requestID) async {
+    var url = Uri.http(domain, '/album-invite', {"id": requestID});
+    final Map<String, String> headers = {'Authorization': 'Bearer $token'};
+
+    try {
+      final response = await http.delete(url, headers: headers);
 
       if (response.statusCode == 200) {
         return true;
