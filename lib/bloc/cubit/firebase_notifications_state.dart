bd087fea9768d93a2eb5c05b94f47614e6465081
Dominick Madonna <dominick.madonn@gmail.com>
2024-05-21 20:28:49 -0400

    Integrated Firebase and Firebase Messaging into application to handle push notifications and possibly deprecate redis notifications.

---
+++ lib/bloc/cubit/firebase_notifications_state.dart
@@ -0,0 +1,9 @@
+part of 'firebase_notifications_cubit.dart';
+
+class FirebaseNotificationsState extends Equatable {
+  const FirebaseNotificationsState();
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => [];
+}
